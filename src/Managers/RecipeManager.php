3140414e7aa4aa52a97713ccba40c53450410bb4
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/RecipeManager.php
+++ src/Managers/RecipeManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Recipe newEntity()
+ * @method \Amethyst\Schemas\RecipeSchema getSchema()
+ * @method \Amethyst\Repositories\RecipeRepository getRepository()
+ * @method \Amethyst\Serializers\RecipeSerializer getSerializer()
+ * @method \Amethyst\Validators\RecipeValidator getValidator()
+ * @method \Amethyst\Authorizers\RecipeAuthorizer getAuthorizer()
+ */
 class RecipeManager extends Manager
 {
     use ConfigurableManager;
