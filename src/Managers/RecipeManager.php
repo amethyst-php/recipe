917191c5955cc2d0c56e77654f67db488e8d4b73
railken <railken@protonmail.ch>
2019-07-14 13:11:28 +0200

    fix style

--- src/Managers/RecipeManager.php
+++ src/Managers/RecipeManager.php
@@ -6,12 +6,12 @@ use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
 /**
- * @method \Amethyst\Models\Recipe newEntity()
- * @method \Amethyst\Schemas\RecipeSchema getSchema()
+ * @method \Amethyst\Models\Recipe                 newEntity()
+ * @method \Amethyst\Schemas\RecipeSchema          getSchema()
  * @method \Amethyst\Repositories\RecipeRepository getRepository()
- * @method \Amethyst\Serializers\RecipeSerializer getSerializer()
- * @method \Amethyst\Validators\RecipeValidator getValidator()
- * @method \Amethyst\Authorizers\RecipeAuthorizer getAuthorizer()
+ * @method \Amethyst\Serializers\RecipeSerializer  getSerializer()
+ * @method \Amethyst\Validators\RecipeValidator    getValidator()
+ * @method \Amethyst\Authorizers\RecipeAuthorizer  getAuthorizer()
  */
 class RecipeManager extends Manager
 {
